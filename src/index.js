6c4f700f8d5a5a02b4be1af96615a998af3eace4
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2020-10-17 23:39:16 +0300

    10 дз

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import menuCardsTpl from './templates/color-card.hbs';
+import menu from './menu.json';
+import './styles.css';
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const STORAGE_KEY = `actual-theme`;
+
+
+const cardsList = document.querySelector(`.js-menu`);
+const toggle = document.querySelector(`.theme-switch__toggle`);
+const body = document.querySelector(`body`);
+
+const cardsMarkUp = createMenuCards(menu);
+
+cardsList.insertAdjacentHTML(`beforeend`, cardsMarkUp);
+
+function createMenuCards(menu){
+    if (localStorage.getItem(STORAGE_KEY) === Theme.DARK){
+        body.classList.add(Theme.DARK);
+        toggle.checked = true;
+    }
+    else {
+        body.classList.add(Theme.LIGHT);
+    }
+    return menuCardsTpl(menu);
+}
+
+toggle.addEventListener(`click`, change);
+
+function change(e){
+    if (localStorage.getItem(STORAGE_KEY) === Theme.DARK){
+        body.classList.remove(Theme.DARK);
+        body.classList.add(Theme.LIGHT);
+        localStorage.removeItem(STORAGE_KEY);
+        localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
+    }
+else {
+    body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        localStorage.removeItem(STORAGE_KEY);
+        localStorage.setItem(STORAGE_KEY, Theme.DARK);
+}
+};
