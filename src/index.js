1fd50e76cea22204520e222717eab5a965356b9d
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2020-10-20 23:57:09 +0300

    правки с удалением локального хранилища

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const toggle = document.querySelector(`.theme-switch__toggle`);
 const body = document.querySelector(`body`);
 
 const cardsMarkUp = createMenuCards(menu);
+// let curentLocalStorage = localStorage.getItem(STORAGE_KEY);
 
 cardsList.insertAdjacentHTML(`beforeend`, cardsMarkUp);
 
@@ -31,17 +32,18 @@ function createMenuCards(menu){
 
 toggle.addEventListener(`click`, change);
 
+
 function change(e){
     if (localStorage.getItem(STORAGE_KEY) === Theme.DARK){
         body.classList.remove(Theme.DARK);
         body.classList.add(Theme.LIGHT);
-        localStorage.removeItem(STORAGE_KEY);
+        // localStorage.removeItem(STORAGE_KEY);
         localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
     }
 else {
     body.classList.remove(Theme.LIGHT);
         body.classList.add(Theme.DARK);
-        localStorage.removeItem(STORAGE_KEY);
+        // localStorage.removeItem(STORAGE_KEY);
         localStorage.setItem(STORAGE_KEY, Theme.DARK);
 }
 };
